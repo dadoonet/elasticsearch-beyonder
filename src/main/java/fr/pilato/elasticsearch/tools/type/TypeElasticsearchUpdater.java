7a2d8dfe77604a637bd31c0a0d6da29312b91467
David Pilato <david@pilato.fr>
2018-02-05 08:45:55 +0100

    Upgrade to latest elasticsearch 5.6

--- src/main/java/fr/pilato/elasticsearch/tools/type/TypeElasticsearchUpdater.java
+++ src/main/java/fr/pilato/elasticsearch/tools/type/TypeElasticsearchUpdater.java
@@ -19,12 +19,12 @@
 
 package fr.pilato.elasticsearch.tools.type;
 
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
 import org.elasticsearch.action.admin.indices.mapping.put.PutMappingResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.http.entity.ContentType;
-import org.elasticsearch.client.http.entity.StringEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
