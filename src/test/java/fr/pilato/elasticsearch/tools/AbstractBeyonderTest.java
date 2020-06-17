d1e7d4cbc9a0f659db68253bb3d29dc62b553039
HJK181 <hjk@commerce-experts.com>
2020-06-17 15:07:11 +0200

    Replace fabric8 docker container by testcontainers

--- src/test/java/fr/pilato/elasticsearch/tools/AbstractBeyonderTest.java
+++ src/test/java/fr/pilato/elasticsearch/tools/AbstractBeyonderTest.java
@@ -19,19 +19,11 @@
 
 package fr.pilato.elasticsearch.tools;
 
-import fr.pilato.elasticsearch.tools.index.IndexSettingsReader;
-import org.apache.http.HttpHost;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.elasticsearch.client.Request;
-import org.elasticsearch.client.Response;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder;
-import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import static java.util.Collections.singletonList;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assume.assumeThat;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -39,68 +31,69 @@ import java.net.ConnectException;
 import java.util.List;
 import java.util.Map;
 
-import static java.util.Collections.singletonList;
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assume.assumeThat;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.Request;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.elasticsearch.ElasticsearchContainer;
+
+import fr.pilato.elasticsearch.tools.index.IndexSettingsReader;
 
 public abstract class AbstractBeyonderTest {
 
     static final Logger logger = LoggerFactory.getLogger(AbstractBeyonderTest.class);
 
-    private final static String DEFAULT_TEST_CLUSTER = "http://127.0.0.1:9400";
-    private final static Integer DEFAULT_TEST_CLUSTER_TRANSPORT_PORT = 9500;
-
-    final static String testCluster = System.getProperty("tests.cluster", DEFAULT_TEST_CLUSTER);
-    final static String testClusterUser = System.getProperty("tests.cluster.user");
-    final static String testClusterPass = System.getProperty("tests.cluster.pass");
-    final static int testClusterTransportPort = Integer.parseInt(System.getProperty("tests.cluster.transport.port", DEFAULT_TEST_CLUSTER_TRANSPORT_PORT.toString()));
-
     abstract protected void testBeyonder(String root,
                                          List<String> indices,
                                          List<String> templates) throws Exception;
 
-    private static RestClient client;
+	protected static ElasticsearchContainer container;
+	protected static RestClient				client;
+	protected static Client					transportClient;
 
-    static RestClient restClient() throws IOException {
-        if (client == null) {
-            startRestClient();
-        }
-        return client;
-    }
+	@BeforeClass
+	public static void initilizeElasticSearch() throws IOException, InterruptedException {
+		container = new ElasticsearchContainer("docker.elastic.co/elasticsearch/elasticsearch:7.7.1");
+		container.start();
+		client = RestClient.builder(HttpHost.create(container.getHttpHostAddress())).build();
+		TransportAddress transportAddress = new TransportAddress(container.getTcpHost());
+		Settings settings = Settings.builder().put("cluster.name", "docker-cluster").build();
+		transportClient = new PreBuiltTransportClient(settings).addTransportAddress(transportAddress);
+		testClusterRunning();
+	}
 
-    private static void startRestClient() throws IOException {
-        if (client == null) {
-            RestClientBuilder builder = RestClient.builder(HttpHost.create(testCluster));
-            if (testClusterUser != null) {
-                final CredentialsProvider credentialsProvider =
-                        new BasicCredentialsProvider();
-                credentialsProvider.setCredentials(AuthScope.ANY,
-                        new UsernamePasswordCredentials(testClusterUser, testClusterPass));
-                builder.setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder
-                        .setDefaultCredentialsProvider(credentialsProvider));
-            }
-
-            client = builder.build();
-            testClusterRunning();
-        }
-    }
-
-    private static boolean testClusterRunning() throws IOException {
-        try {
-            Response response = client.performRequest(new Request("GET", "/"));
-            Map<String, Object> asMap = (Map<String, Object>) JsonUtil.asMap(response).get("version");
-            logger.info("Starting integration tests against an external cluster running elasticsearch [{}]", asMap.get("number"));
-            return false;
-        } catch (ConnectException e) {
-            // If we have an exception here, let's ignore the test
-            logger.warn("Integration tests are skipped: [{}]", e.getMessage());
-            assumeThat("Integration tests are skipped", e.getMessage(), not(containsString("Connection refused")));
-            return false;
-        } catch (IOException e) {
-            logger.error("Full error is", e);
-            throw e;
-        }
-    }
+	@SuppressWarnings("unchecked")
+	private static boolean testClusterRunning() throws IOException {
+		try {
+			Response response = client.performRequest(new Request("GET", "/"));
+			Map<String, Object> asMap = (Map<String, Object>) JsonUtil.asMap(response).get("version");
+			logger.info("Starting integration tests against an external cluster running elasticsearch [{}]", asMap.get("number"));
+			return false;
+		} catch (ConnectException e) {
+			// If we have an exception here, let's ignore the test
+			logger.warn("Integration tests are skipped: [{}]", e.getMessage());
+			assumeThat("Integration tests are skipped", e.getMessage(), not(containsString("Connection refused")));
+			return false;
+		} catch (IOException e) {
+			logger.error("Full error is", e);
+			throw e;
+		}
+	}
+	
+	@AfterClass
+	public static void tearDownElasticSearch() {
+		if (container != null)
+			container.stop();
+	}
 
     @Test
     public void testDefaultDir() throws Exception {
@@ -118,7 +111,8 @@ public abstract class AbstractBeyonderTest {
                 null);
     }
 
-    @Test
+    @SuppressWarnings("unchecked")
+	@Test
     public void testVariableReplacement() throws Exception {
 
       // given: A settings json with a variable that should be replaced.
@@ -131,7 +125,7 @@ public abstract class AbstractBeyonderTest {
       Map<String, Object> settingsMap = JsonUtil.asMap(new ByteArrayInputStream(settings.getBytes()));
 
       // then: the variables got replaced by environment variables of the same name
-      String numberOfReplicas = (String) ((Map) settingsMap.get("settings")).get("number_of_replicas");
+      String numberOfReplicas = (String) ((Map<String, Object>) settingsMap.get("settings")).get("number_of_replicas");
       assumeThat(numberOfReplicas, equalTo("2"));
 
     }
